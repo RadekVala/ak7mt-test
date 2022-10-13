e0eed30497fc3f0e6d02ecb273de5dad863c5df4
Radek Vala <radek.www@gmail.com>
2022-10-13 16:13:13 +0200

    SecondActivity.kt - use view binding in activity - counter implementation added

--- app/src/main/java/cz/utb/fai/demoapp/SecondActivity.kt
+++ app/src/main/java/cz/utb/fai/demoapp/SecondActivity.kt
@@ -2,10 +2,26 @@ package cz.utb.fai.demoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cz.utb.fai.demoapp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    var count = 0;
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvCount.text = count.toString()
+
+
+        binding.btnClicker.setOnClickListener {
+            count++
+            binding.tvCount.text = count.toString()
+        }
+
     }
 }
